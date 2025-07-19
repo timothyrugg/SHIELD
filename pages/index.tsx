5b353aa0d3daa7662730cd57ab3b8bc126533b8e
timothyrugg <timothyrugg@gmail.com>
2025-07-19 12:15:50 -0400

    Updated Pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import Head from 'next/head';
 
-export default function Home() {
+export default function Landing() {
   return (
-    <div className="h-screen flex flex-col justify-center items-center bg-black text-white">
+    <div className="relative h-screen bg-black text-white flex flex-col justify-center items-center text-center">
       <Head>
         <title>Shield and Ladder</title>
       </Head>
-      <h1 className="text-4xl font-bold">United by Service. Driven by Endurance.</h1>
-      <a href="/home" className="mt-6 px-6 py-3 bg-white text-black rounded-xl">Enter</a>
+      <h1 className="text-4xl font-bold mb-4">United by Service. Driven by Endurance.</h1>
+      <a href="/home" className="px-6 py-3 bg-white text-black rounded-lg font-medium hover:bg-gray-200 transition">
+        Enter
+      </a>
     </div>
   );
 }
